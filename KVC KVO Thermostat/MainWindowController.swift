854b96b9804727dddbd711bd4521447bae1ee827
Laurence Wingo <laurencewingo@laurences-macbook-air.local>
2018-05-19 15:46:36 -0400

    Implement UI Design for KVC KVO Thermostat
    I'll be using KVC and KVO as a shoreline to have the UI objects interact with each other based on changes in value.

--- KVC KVO Thermostat/MainWindowController.swift
+++ KVC KVO Thermostat/MainWindowController.swift
@@ -9,10 +9,19 @@
 import Cocoa
 
 class MainWindowController: NSWindowController {
+    
+    var temperature = 75
+    
+    
+    
+    //override the windowNibName because I used the parameter-less constructor to create the instance of MainWindowController in the appDelegate therefore I must provide the name of the NIB file in this class
+    //how is this possible that I'm able to delcare it as a string if the property is of type NSNib.Name?
+    override var windowNibName: NSNib.Name {
+        return NSNib.Name("MainWindowController")
+    }
 
     override func windowDidLoad() {
         super.windowDidLoad()
-
         // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
     }
     
