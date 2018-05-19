b17d7d55005b03fb50c968a9e61ac0209340b95f
Laurence Wingo <laurencewingo@laurences-macbook-air.local>
2018-05-19 14:57:02 -0400

    Initial Commit

---
+++ KVC KVO Thermostat/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  KVC KVO Thermostat
+//
+//  Created by Laurence Wingo on 5/19/18.
+//  Copyright © 2018 Cosmic Arrows, LLC. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
